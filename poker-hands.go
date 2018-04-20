ac1699acf2dfd657c9c8791f8cd185572deafab5
yegorov-boris <borisyegorov8@gmail.com>
2018-04-20 10:36:11 +0300

    Create several goroutines

--- poker-hands.go
+++ poker-hands.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"bufio"
+	//"bufio"
 )
 
 func main() {
@@ -22,14 +22,40 @@ func main() {
 
 	defer resp.Body.Close()
 
-	var i = 0;
-	scanner := bufio.NewScanner(resp.Body)
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-		i++
-		if (i == chunkSize) {
-			fmt.Println("------------")
-			i = 0
-		}
+	//var i = 0;
+	//scanner := bufio.NewScanner(resp.Body)
+	//for scanner.Scan() {
+	//	fmt.Println(scanner.Text())
+	//	i++
+	//	if (i == chunkSize) {
+	//		fmt.Println("------------")
+	//		i = 0
+	//	}
+	//}
+
+	var inputs [chunkSize]chan string
+	var outputs [chunkSize]chan bool
+	for i := range inputs {
+		inputs[i] = make(chan string)
+		outputs[i] = make(chan bool)
+		createChecker(inputs[i], outputs[i]);
+		inputs[i] <- "foo"
 	}
+
+	for i := range inputs {
+		fmt.Println(<- outputs[i])
+	}
+}
+
+func createChecker(input chan string, output chan bool) {
+	go func() {
+		for {
+			msg := <- input
+			if msg == "foo" {
+				output <- true
+			} else {
+				output <- false
+			}
+		}
+	}()
 }
